455589a5b3e00f46799584297b23e6bcb79263c3
adarshsharma3 <136933675+adarshsharma3@users.noreply.github.com>
2024-07-17 18:05:07 +0530

    Add files via upload

---
+++ src/pages/SignInAssets.jsx
@@ -0,0 +1,11 @@
+import signinBg from './images/SignIn.jpg';
+import signupBg from './images/SignUp.jpg';
+
+const assets = {
+  images: {
+    signinBg: signinBg,
+    signupBg: signupBg
+  }
+};
+
+export default assets;
